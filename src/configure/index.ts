e3fda68d3690359376097849cc5fbf9cd0cfab46
Michael Wagner <michael.wagner@allaboutapps.at>
2020-06-29 12:11:40 +0200

    config: Add env config vars for prod & test

--- src/configure/index.ts
+++ src/configure/index.ts
@@ -1,4 +1,9 @@
 export const CONFIG = {
+    env: {
+        isProduction: process.env.NODE_ENV === 'production',
+        isTest: process.env.NODE_ENV === 'test'
+    },
+    
     auth: {
         tokenValidity: parseInt(process.env.TOKEN_VALIDITY, 10) || 86400000,
         refreshTokenValidityMS: parseInt(process.env.REFRESH_TOKEN_VALIDITY, 10) || 0, // default 0 = unlimited
