77a9e5f045306137cdbbc136dc99e046f8fd1a4c
Michael Wagner <michael.wagner@allaboutapps.at>
2021-10-07 14:17:29 +0200

    fix: solve exit code 1 issue with running the test in a sub shell, because of unknown failing reason
    https://github.com/facebook/jest/issues/9324

--- docker-dev-helper.sh
+++ docker-dev-helper.sh
@@ -8,7 +8,19 @@ if [ "$1" = "--up" ]; then
 fi
 
 if [ "$1" = "--test" ]; then
-    docker-compose -f 'docker-compose.dev.yml' exec app yarn env-cmd -f ./env/test.docker.env jest --detectOpenHandles --runInBand
+    declare -fx _Test_Method
+    _Test_Method() {
+        docker-compose -f 'docker-compose.dev.yml' exec app yarn env-cmd -f ./env/test.docker.env jest --runInBand --ci
+    }
+    
+    ( _Test_Method; )
+    ERROR_CODE=$?
+    echo $ERROR_CODE
+    if [[ $ERROR_CODE -eq 0 ]]; then 
+        exit 0;
+    else
+        exit 1;
+    fi
 fi
 
 if [ "$1" = "--bash" ]; then
